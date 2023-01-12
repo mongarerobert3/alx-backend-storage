e7da927185715a262d63552458119363860643bc
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-12 15:24:30 +0300

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,8 @@
---script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (IFNULL(SPLIT, '2020') - formed) AS lifespan
-    FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+
+SELECT
+    band_name AS band_name,
+    IFNULL(split, CURRENT_DATE()) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
