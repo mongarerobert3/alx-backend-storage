491e33e065f221794870c6ea173287b1a2883ab3
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-12 12:34:14 +0300

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---creates a unique table
-DROP TABLE IF EXISTS users ;
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT NOT NULL AUTO INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
