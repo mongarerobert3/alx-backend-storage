5ccbcf5663f5ee860bdc5ee792cbb503bedc02f7
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-12 22:14:04 +0300

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,7 @@
 --ranks country of origin bands
-SELECT origin, SUM(fans) AS nb_fans
-    FROM metal_bands
-    GROUP BY origin
-    ORDER BY nb_fans DESC;
+SELECT
+    origin,
+    SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
