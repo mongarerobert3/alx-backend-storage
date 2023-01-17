3bd54ebef9af74226502c3b8a5ca21199b33882f
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-17 22:22:39 +0300

    Change school topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,7 +2,7 @@
 """changes all topics of a school document based on the name"""
 
 
-def insert_school(mongo_collection, **kwargs):
+def update_topics(mongo_collection, name, topics):
     """changes all topics of a school document based on the name"""
     mongo_collection.update_many(
         {'name': name},
