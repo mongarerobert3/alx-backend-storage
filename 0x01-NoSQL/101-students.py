d9827fdcf3b29ad516ea39b19cf410ca9210a7a8
mongarerobert3 <mongarerobert3@gmail.com>
2023-01-17 22:31:21 +0300

    Advanced

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
-"""Module for task 101
+"""
+students
 """
 
 
 def top_students(mongo_collection):
-    """Sorts list"""
-    stud = mongo_collection.aggregate(
-        [
-            {
-                '$project': {
-                    '_id': 1,
-                    'name': 1,
-                    'averageScore': {
-                        '$avg': {
-                            '$avg': '$topics.score',
-                        },
-                    },
-                    'topics': 1,
-                },
-            },
-            {
-                '$sort': {'averageScore': -1}
-            },
-        ]
-    )
-    return 
+    """ students by score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
